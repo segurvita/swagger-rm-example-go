82502cbf4711d68c48bafbeb2bb7215a88fd5dc8
segur <segur.opus@gmail.com>
2019-01-07 17:12:03 +0900

    Add example function for test to swagger.go

--- lib/swagger/swagger.go
+++ lib/swagger/swagger.go
@@ -1,7 +1,19 @@
 package swagger
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
+// practice of println
 func HelloWorld() {
 	fmt.Println("Hello World!")
+}
+
+// practice of test
+func example(code string) (int, error) {
+	if code == "hoge" {
+		return 1, nil
+	}
+	return 0, errors.New("code must be hoge")
 }
